baefeb99260545c37791465a274ea2d1c43723e1
Gabriel Fernandez <hotdesc@gmail.com>
2021-06-27 16:21:45 -0400

    rotated text method added

--- src/Fpdi.php
+++ src/Fpdi.php
@@ -261,6 +261,14 @@ class Fpdi extends FpdfTpl
         }
     }
 
+    function RotatedText($x, $y, $txt, $angle)
+    {
+        //Text rotated around its origin
+        $this->Rotate($angle,$x,$y);
+        $this->Text($x,$y,$txt);
+        $this->Rotate(0);
+    }
+
     function _endpage()
     {
         if($this->angle!=0)
